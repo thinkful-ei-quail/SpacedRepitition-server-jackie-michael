5290a55b16ece9096fd44f3712be542ae2079ebe
Jackie Abert <jax.abert@gmail.com>
2020-10-05 10:54:23 -0400

    seed table work

--- seeds/seed.tables.sql
+++ seeds/seed.tables.sql
@@ -17,18 +17,20 @@ VALUES
 
 INSERT INTO "language" ("id", "name", "user_id")
 VALUES
-  (1, 'French', 1);
+  (1, 'Italian', 1);
 
 INSERT INTO "word" ("id", "language_id", "original", "translation", "next")
 VALUES
-  (1, 1, 'entraine toi', 'practice', 2),
-  (2, 1, 'bonjour', 'hello', 3),
-  (3, 1, 'maison', 'house', 4),
-  (4, 1, 'développeur', 'developer', 5),
-  (5, 1, 'traduire', 'translate', 6),
-  (6, 1, 'incroyable', 'amazing', 7),
-  (7, 1, 'chien', 'dog', 8),
-  (8, 1, 'chat', 'cat', null);
+  (1, 1, 'cane', 'dog', 2),
+  (2, 1, 'gatto', 'cat', 3),
+  (3, 1, 'blu', 'blue', 4),
+  (4, 1, 'rosso', 'red', 5),
+  (5, 1, 'giallo', 'yellow', 6),
+  (6, 1, 'birra', 'beer', 7),
+  (7, 1, 'colazione', 'breakfast', 8),
+  (8, 1, 'formaggio', 'cheese', 9);
+  (9, 1, 'padre', 'father', 10);
+  (10, 1, 'madre', 'mother', null);
 
 UPDATE "language" SET head = 1 WHERE id = 1;
 
